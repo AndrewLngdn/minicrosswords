ce49c7b8bfb281c032357ca4f9aaf7a40ac7a1e7
Andrewlngdn <andrewlngdn@gmail.com>
2018-02-15 15:35:50 -0800

    add clues to repo

--- maker.py
+++ maker.py
@@ -1,3 +1,4 @@
+import csv;
 
 def print_grid(grid):
   for row in grid:
@@ -13,3 +14,7 @@ grid[2] = ["S", "P", "U", "D"]
 print_grid(grid);
 
 
+with open("clues.csv") as clues_csv:
+  csv_reader = csv.reader(clues_csv)
+  for row in csv_reader:
+    print(row)
