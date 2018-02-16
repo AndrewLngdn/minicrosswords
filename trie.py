cdac18dd3a1532617a26df8b48566a871d0c9cb6
Andrewlngdn <andrewlngdn@gmail.com>
2018-02-15 18:17:38 -0800

    add prefix method

--- trie.py
+++ trie.py
@@ -5,12 +5,14 @@ class Trie:
     for word in word_list:
       self.root.add_word(word);
 
+  # def words_with_prefix(self, prefix, length=None):
 
 class TrieNode:
-  def __init__(self, character=""):
+  def __init__(self, character="", parent=None):
     print character
     self.character = character;
     self.children = {}
+    self.parent = parent
 
   def add_word(self, word):
     if word is '':
@@ -22,12 +24,17 @@ class TrieNode:
     # print "rest       " + str(rest)
 
     if first_char not in self.children:
-      self.children[first_char] = TrieNode(first_char)
+      self.children[first_char] = TrieNode(first_char, self)
 
     self.children[first_char].add_word(rest)
+
+  def prefix(self, suffix=""):
+    if self.character is '':
+      return suffix
+    last_char = self.character
+    self.parent.prefix(last_char + suffix)
 
 
 if __name__ == "__main__":
   trie = Trie(["hi", "hello", "howareyou", "helloworld"])
-  # import pdb; pdb.set_trace()
 
