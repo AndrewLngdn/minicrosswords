f3e9fb7a96aed80648098b592dbdbf1f557bfb52
Andrewlngdn <andrewlngdn@gmail.com>
2018-02-15 18:45:14 -0800

    add words_from_node

--- trie.py
+++ trie.py
@@ -5,7 +5,12 @@ class Trie:
     for word in word_list:
       self.root.add_word(word);
 
-  # def words_with_prefix(self, prefix, length=None):
+  def words_with_prefix(self, prefix, length=None):
+    prefix_node = self.root
+    for chars in prefix: 
+      prefix_node = node.children[char]
+
+
 
 class TrieNode:
   def __init__(self, character="", parent=None):
@@ -13,6 +18,7 @@ class TrieNode:
     self.character = character;
     self.children = {}
     self.parent = parent
+    # self.is_word = True
 
   def add_word(self, word):
     if word is '':
@@ -31,10 +37,27 @@ class TrieNode:
   def prefix(self, suffix=""):
     if self.character is '':
       return suffix
-    last_char = self.character
-    self.parent.prefix(last_char + suffix)
+    else:
+      last_char = self.character
+      return self.parent.prefix(last_char + suffix)
+
+  def words_from_node(self, word=""):
+    if len(self.children) is 0:
+      print word + self.character
+      return word
+    
+    word = word + self.character
+    for char, node in self.children.iteritems():
+      node.words_from_node(word)
+
 
 
 if __name__ == "__main__":
   trie = Trie(["hi", "hello", "howareyou", "helloworld"])
+  assert trie.root.children['h'].children['e'].children['l'].prefix() == "hel"
+  # print trie.root.words_from_node()
+  import pdb; pdb.set_trace();
+
+
+
 
